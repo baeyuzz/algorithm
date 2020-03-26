f24a7bf0d46ea9f27d9582b6a74bbdf38ee1f1e4
배유진 <seow0124@naver.com>
2020-03-26 09:32:33 +0000

    Replace Solution_7576_토마토_배유진.java

--- 0214/Solution_7576_토마토_배유진.java
+++ 0214/Solution_7576_토마토_배유진.java
@@ -1,110 +1,122 @@
-import java.util.Scanner;
+package ssafy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Solution_7576_토마토_배유진 {
-
-	static int n, m, map[][], cnt;
+	static int n, m, map[][], cnt, day = 1;
 	static boolean visited[][];
+	static boolean endcheck = false;
+	static Queue<Point> q = new LinkedList<Point>();
+	static int dx[] = { -1, 1, 0, 0 };
+	static int dy[] = { 0, 0, 1, -1 };
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		m = sc.nextInt();
-		n = sc.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
 
 		map = new int[n][m];
 		visited = new boolean[n][m];
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				map[i][j] = sc.nextInt();
-				if (map[i][j] != 0) {
-					visited[i][j] = true;
+		for (int x = 0; x < n; x++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int y = 0; y < m; y++) {
+				map[x][y] = Integer.parseInt(st.nextToken());
+				if (map[x][y] == -1) {
+					visited[x][y] = true;
 				}
-
 			}
 		}
+
+		// input
+/////////////////////////////////////////////////////
 //		for (int i = 0; i < n; i++) {
 //			for (int j = 0; j < m; j++) {
-//				System.out.print(map[i][j] + "   ");
-//
+//				System.out.print(map[i][j]);
 //			}
 //			System.out.println();
 //		}
+/////////////////////////////////////////////////////
 
-		boolean check = false;
+		for (int x = 0; x < n; x++) {
+			for (int y = 0; y < m; y++) {
+				if (map[x][y] == 1) {
+					q.add(new Point(x, y));
+					visited[x][y] = true;
+				}
+			}
+		}
+
+		bfs();
+//
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 0; j < m; j++) {
+//				System.out.print(map[i][j]);
+//			}
+//			System.out.println();
+//		}
+//
+
 		L: for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				check = dfs();
-				if (check)
+				if (!visited[i][j]) {
+					day = 1;
 					break L;
+				}
 			}
 
 		}
-		System.out.println(cnt);
+		System.out.println(day - 2);
 
 	}
 
-	private static boolean dfs() {
-		boolean check = false;
-		int dx[] = { -1, 1, 0, 0 };
-		int dy[] = { 0, 0, -1, 1 };
+	private static void bfs() {
+		while (!q.isEmpty()) {
+			int size = q.size();
 
-		int tmp = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (visited[i][j])
-					tmp++;
-			}
-		}
-		if (tmp == n * m) {
-			return true;
-		}
+			day++;
 
-		int two = 0;
+			for (int i = 0; i < size; i++) {
+				Point front = q.poll();
+				int fx = front.x;
+				int fy = front.y;
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (map[i][j] == 1 && visited[i][j]) {
-					two = 0;
-					for (int d = 0; d < 4; d++) {
-						int x = i + dx[d];
-						int y = j + dy[d];
-						if (x < 0 || y < 0 || x >= n || y >= m) {
-							two++;
-						} else {
-							if (map[x][y] == 0 && !visited[x][y]) {
-								map[x][y] = 1;
-								check = true;
-								// visited[x][y] = true;
-							}
-							if (map[x][y] == -1) {
-								two++;
-							}
+				for (int d = 0; d < 4; d++) {
+					fx += dx[d];
+					fy += dy[d];
 
-						}
-						x -= dx[d];
-						y -= dy[d];
+					if (fx < n && fx >= 0 && fy < m && fy >= 0 && !visited[fx][fy]) {
+						visited[fx][fy] = true;
+						map[fx][fy] = day;
+						q.add(new Point(fx, fy));
 					}
 
-					if (two == 4) {
-						cnt = -1;
-						return true;
-
-					}
+					fx -= dx[d];
+					fy -= dy[d];
 
 				}
 			}
 		}
-		if (check)
-			cnt++;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (map[i][j] == 1)
-					visited[i][j] = true;
-			}
-		}
+	}
+}
 
-		return false;
+class Point {
+	int x;
+	int y;
+
+	public Point() {
+
 	}
 
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 }
